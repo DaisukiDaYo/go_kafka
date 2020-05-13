26d43b579767918510d094cc1206cfe6f6949a6b
Under <un@odds.team>
2020-05-13 12:03:36 +0700

    Add func CheckKafkaLogWriter 💉

--- multiplelogdemo/multiplelogdemo.go
+++ multiplelogdemo/multiplelogdemo.go
@@ -14,23 +14,39 @@ type stdLogger struct {
 }
 
 var (
-	ReverseFirstWriter    stdLogger
-	ReverseSecondWriter   stdLogger
-	ReverseThirdWriter    stdLogger
-	ForcepostFirstWriter  stdLogger
-	ForcepostSecondWriter stdLogger
-	ForcepostThirdWriter  stdLogger
-	reverseWriter         stdLogger
-	forcepostWriter       stdLogger
+	ReverseFirstWriter     stdLogger
+	ReverseSecondWriter    stdLogger
+	ReverseThirdWriter     stdLogger
+	ReverseForthWriter     stdLogger
+	ReverseFifthWriter     stdLogger
+	ReverseSixthWriter     stdLogger
+	ReverseSeventhWriter   stdLogger
+	ForcepostFirstWriter   stdLogger
+	ForcepostSecondWriter  stdLogger
+	ForcepostThirdWriter   stdLogger
+	ForcepostForthWriter   stdLogger
+	ForcepostFifthWriter   stdLogger
+	ForcepostSixthWriter   stdLogger
+	ForcepostSeventhWriter stdLogger
+	reverseWriter          stdLogger
+	forcepostWriter        stdLogger
 
-	reverseFirstLogger    *log.Logger
-	reverseSecondLogger   *log.Logger
-	reverseThirdLogger    *log.Logger
-	forcepostFirstLogger  *log.Logger
-	forcepostSecondLogger *log.Logger
-	forcepostThirdLogger  *log.Logger
-	reverseLogger         *log.Logger
-	forcepostLogger       *log.Logger
+	reverseFirstLogger     *log.Logger
+	reverseSecondLogger    *log.Logger
+	reverseThirdLogger     *log.Logger
+	reverseForthLogger     *log.Logger
+	reverseFifthLogger     *log.Logger
+	reverseSixthLogger     *log.Logger
+	reverseSeventhLogger   *log.Logger
+	forcepostFirstLogger   *log.Logger
+	forcepostSecondLogger  *log.Logger
+	forcepostThirdLogger   *log.Logger
+	forcepostForthLogger   *log.Logger
+	forcepostFifthLogger   *log.Logger
+	forcepostSixthLogger   *log.Logger
+	forcepostSeventhLogger *log.Logger
+	reverseLogger          *log.Logger
+	forcepostLogger        *log.Logger
 )
 
 type LogConfig struct {
@@ -49,6 +65,69 @@ func generateLumberjack(c *LogConfig) *lumberjack.Logger {
 	}
 }
 
+func CheckKafkaLogWriter() (*log.Logger, *log.Logger, stdLogger, stdLogger) {
+	// Can we use switch case to switching log writer?
+	switch time.Now().Weekday() {
+	case time.Monday:
+		fmt.Println("Use 1 writer")
+		reverseWriter = ReverseFirstWriter
+		reverseLogger = reverseFirstLogger
+		forcepostWriter = ForcepostFirstWriter
+		forcepostLogger = forcepostFirstLogger
+		ReverseSeventhWriter.Close()
+		ForcepostSeventhWriter.Close()
+	case time.Tuesday:
+		fmt.Println("Use 2 writer")
+		reverseWriter = ReverseSecondWriter
+		reverseLogger = reverseSecondLogger
+		forcepostWriter = ForcepostSecondWriter
+		forcepostLogger = forcepostSecondLogger
+		ReverseFirstWriter.Close()
+		ForcepostFirstWriter.Close()
+	case time.Wednesday:
+		fmt.Println("Use 3 writer")
+		reverseWriter = ReverseThirdWriter
+		reverseLogger = reverseThirdLogger
+		forcepostWriter = ForcepostThirdWriter
+		forcepostLogger = forcepostThirdLogger
+		ReverseSecondWriter.Close()
+		ForcepostSecondWriter.Close()
+	case time.Thursday:
+		fmt.Println("Use 4 writer")
+		reverseWriter = ReverseForthWriter
+		reverseLogger = reverseForthLogger
+		forcepostWriter = ForcepostForthWriter
+		forcepostLogger = forcepostForthLogger
+		ReverseThirdWriter.Close()
+		ForcepostThirdWriter.Close()
+	case time.Friday:
+		fmt.Println("Use 5 writer")
+		reverseWriter = ReverseFifthWriter
+		reverseLogger = reverseFifthLogger
+		forcepostWriter = ForcepostFifthWriter
+		forcepostLogger = forcepostFifthLogger
+		ReverseForthWriter.Close()
+		ForcepostForthWriter.Close()
+	case time.Saturday:
+		fmt.Println("Use 6 writer")
+		reverseWriter = ReverseSixthWriter
+		reverseLogger = reverseSixthLogger
+		forcepostWriter = ForcepostSixthWriter
+		forcepostLogger = forcepostSixthLogger
+		ReverseFifthWriter.Close()
+		ForcepostFifthWriter.Close()
+	case time.Sunday:
+		fmt.Println("Use 7 writer")
+		reverseWriter = ReverseSeventhWriter
+		reverseLogger = reverseSeventhLogger
+		forcepostWriter = ForcepostSeventhWriter
+		forcepostLogger = forcepostSeventhLogger
+		ReverseSixthWriter.Close()
+		ForcepostSixthWriter.Close()
+	}
+	return reverseLogger, forcepostLogger, reverseWriter, forcepostWriter
+}
+
 func (c *LogConfig) SetLogger() (*log.Logger, *log.Logger, stdLogger, stdLogger) {
 	fmt.Printf("Sending log messages to: %s\n", c.Logfile)
 	reverseLumberjack := generateLumberjack(c)
@@ -62,22 +141,22 @@ func (c *LogConfig) SetLogger() (*log.Logger, *log.Logger, stdLogger, stdLogger)
 	ReverseSecondWriter = stdLogger{reverseSecondLumberjack}
 
 	reverseThirdLumberjack := generateLumberjack(c)
-	reverseThirdLogger := log.New(reverseThirdLumberjack, "", 1)
+	reverseThirdLogger = log.New(reverseThirdLumberjack, "", 1)
 	reverseThirdLogger.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 	ReverseThirdWriter = stdLogger{reverseThirdLumberjack}
 
 	forcepostLumberjack := generateLumberjack(c)
-	forcepostFirstLogger := log.New(forcepostLumberjack, "", 1)
+	forcepostFirstLogger = log.New(forcepostLumberjack, "", 1)
 	forcepostFirstLogger.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 	ForcepostFirstWriter = stdLogger{forcepostLumberjack}
 
 	forcepostSecondLumberjack := generateLumberjack(c)
-	forcepostSecondLogger := log.New(forcepostSecondLumberjack, "", 1)
+	forcepostSecondLogger = log.New(forcepostSecondLumberjack, "", 1)
 	forcepostSecondLogger.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 	ForcepostSecondWriter = stdLogger{forcepostSecondLumberjack}
 
 	forcepostThirdLumberjack := generateLumberjack(c)
-	forcepostThirdLogger := log.New(forcepostThirdLumberjack, "", 1)
+	forcepostThirdLogger = log.New(forcepostThirdLumberjack, "", 1)
 	forcepostThirdLogger.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 	ForcepostThirdWriter = stdLogger{forcepostThirdLumberjack}
 
@@ -106,33 +185,7 @@ func main() {
 		LogKafkaMessageToFile(fileName, txStr, podID, forcepostWriter, forcepostLogger)
 	}
 
-	t := time.Now()
-	rounded := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
-	fmt.Println(t)
-	fmt.Println(rounded)
-	if true {
-		reverseWriter = ReverseSecondWriter
-		reverseLogger = reverseSecondLogger
-		ReverseFirstWriter.Close()
-	}
-
-	// Can we use switch case to switching log writer?
-	switch time.Now().Weekday() {
-	case time.Monday:
-		fmt.Println("Use 1 writer")
-	case time.Tuesday:
-		fmt.Println("Use 2 writer")
-	case time.Wednesday:
-		fmt.Println("Use 3 writer")
-	case time.Thursday:
-		fmt.Println("Use 4 writer")
-	case time.Friday:
-		fmt.Println("Use 5 writer")
-	case time.Saturday:
-		fmt.Println("Use 6 writer")
-	case time.Sunday:
-		fmt.Println("Use 7 writer")
-	}
+	reverseLogger, forcepostLogger, reverseWriter, forcepostWriter = CheckKafkaLogWriter()
 
 	fileName = "app/kafka_error_logs/reverse_rotate_tmr_%s.txt"
 	txStr = "3test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file end"
@@ -141,16 +194,11 @@ func main() {
 		LogKafkaMessageToFile(fileName, txStr, podID, reverseWriter, reverseLogger)
 	}
 
-	if true {
-		reverseWriter = ReverseFirstWriter
-		reverseLogger = reverseFirstLogger
-	}
-
-	fileName = "app/kafka_error_logs/reverse_rotate_3_%s.txt"
+	fileName = "app/kafka_error_logs/force_post_rotate_tmr_%s.txt"
 	txStr = "4test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file test rolling file end"
-	podID = "1243"
+	podID = "1245"
 	for i := 1; i <= 105000; i++ {
-		LogKafkaMessageToFile(fileName, txStr, podID, reverseWriter, reverseLogger)
+		LogKafkaMessageToFile(fileName, txStr, podID, forcepostWriter, forcepostLogger)
 	}
 }
 
@@ -175,6 +223,5 @@ type LogKafkaMessageToFileFunc func(string, string, string)
 
 func LogKafkaMessageToFile(fileName string, txStr string, podID string, stdLogger stdLogger, log *log.Logger) {
 	cleanFileName := GetFileNameFormat(podID, fileName)
-	// fmt.Println(cleanFileName)
 	WriteLogFile(cleanFileName, txStr, stdLogger, log)
 }
